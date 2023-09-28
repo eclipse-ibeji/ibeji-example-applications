b4c975702b678935584a443b37da27bd5f13da9f
William Lyles <26171886+wilyle@users.noreply.github.com>
2023-09-28 15:56:46 -0700

    fmt

--- freyja_apps/e2e/src/main.rs
+++ freyja_apps/e2e/src/main.rs
@@ -2,8 +2,8 @@
 // Licensed under the MIT license.
 // SPDX-License-Identifier: MIT
 
-use ibeji_adapter::ibeji_adapter::IbejiAdapter;
 use azure_cloud_connector_adapter::azure_cloud_connector_adapter::AzureCloudConnectorAdapter;
+use ibeji_adapter::ibeji_adapter::IbejiAdapter;
 use in_memory_mock_mapping_client::in_memory_mock_mapping_client::InMemoryMockMappingClient;
 
 freyja::freyja_main! {
